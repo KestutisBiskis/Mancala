1e3adcffea2902249290e18ee218d4a2f1bfb73b
codestutis <kgb1043@usnh.edu>
2025-01-08 15:59:09 -0500

    idk bruh

--- src/test/java/com/codestutis/mancala/MancalaTester.java
+++ src/test/java/com/codestutis/mancala/MancalaTester.java
@@ -1,5 +1,10 @@
 package src.test.java.com.codestutis.mancala;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MancalaTester {
+    @Test
+    void compSkip(){
 
+    }
 }
